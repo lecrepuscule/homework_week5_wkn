c98be170b8180f17d210f2bc27e71ed5ec40cd41
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-13 17:49:23 +0100

    added trip details

--- uber_loomper/app/controllers/journeys_controller.rb
+++ uber_loomper/app/controllers/journeys_controller.rb
@@ -11,9 +11,23 @@ class JourneysController < ApplicationController
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @journey = Journey.new(journey_params)
+    if journey.save
+      redirect_to @journey
+    else
+      redirect_to new_user_journey_path
+    end
+  end
+
+  def show
+    @journey = Journey.find(params[:id])
   end
 
 
-
+private
+  def journey_params
+    params.require(:journey).permit!
+  end
 
 end
