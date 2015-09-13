46a6954c695e0d7979a0d0309d45877a5e951da8
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-13 11:10:13 +0100

    added seeding data

--- uber_loomper/db/seeds.rb
+++ uber_loomper/db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Fabricator(:user) do
+  first_name { Faker::Name.first_name }
+  last_name { Faker::Name.last_name }
+  email {Faker::Internet.email}
+  phone {Faker::Number.number(8)}
+end
+
+Fabricator(:vehicle) do
+  model { "mercedes-benz" }
+  licence_plate { Faker::Number.hexadecimal(6) }
+  capacity {Faker::Number.between(3,10)}
+  available {true}
+  status {"active"}
+end
+
+Fabricator(:driver) do
+  first_name { Faker::Name.first_name }
+  last_name { Faker::Name.last_name }
+  employee_id {Faker::Number.number(8)}
+  address {Faker::Address.street_address}
+  available {true}
+end
+
+
+10.times {Fabricate(:vehicle)}
+10.times {Fabricate(:driver)}
+50.times do 
+  Fabricate(:user).journeys.create(
+    pickup_location: Faker::Address.street_address,
+    destination: Faker::Address.street_address,
+    pickup_time: Faker::Time.backward(5, :all),
+    arrival_time: Faker::Time.backward(5, :all),
+    num_of_passengers: Faker::Number.between(1,8),
+    driver: Driver.find(Random.new.rand(1..10)),
+    vehicle: Vehicle.find(Random.new.rand(1..10))
+  )
+end
+
